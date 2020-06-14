eecea70f18b82f538349598317aac93aff917ae5
Matt Kilcup <nevermindthelabel@gmail.com>
2020-06-14 16:49:22 -0700

    updates to server

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const cookieParser = require('cookie-parser');
 const csrf = require('csurf');
 const PORT = process.env.PORT || 3001;
 
-const app = express();
+const csrfProtection = csrf({ cookie: true });
 
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('home route')
