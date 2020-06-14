170e746b223def0255db7dab8188681cd355a664
Matt Kilcup <nevermindthelabel@gmail.com>
2020-06-14 16:33:12 -0700

    server updates

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('home route')
+});
+
 app.listen(PORT, () => {
   console.log(`API server running on port http://localhost:${PORT}`)
 });
